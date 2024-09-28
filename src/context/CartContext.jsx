af9cb05e93929b1412e1dc7ff964b1e454424de1
gonrodca <gonzalo-r@hotmail.es>
2024-09-27 21:42:27 -0300

    pr final

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,13 +4,36 @@ import { createContext } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartState, setCartState] = useState(0);
+  const [cartState, setCartState] = useState([]);
 
-  const addItem = (product) => {
-    setCartState(cartState + 1);
+  const addItem = (product, qtyItem) => {
+    const existeProducto = cartState.some((item) => item.id === product.id);
+    if (existeProducto) {
+      setCartState(
+        cartState.map((item) =>
+          item.id === product.id ? { ...item, qtyItem: item.qtyItem + 1 } : item
+        )
+      );
+    } else {
+      setCartState([...cartState, { ...product, qtyItem }]);
+    }
   };
   const removeItem = (product) => {
-    setCartState(cartState + 1);
+    const exist = cartState.some((item) => item.id === product.id);
+    if (exist) {
+      const prd = cartState.find((item) => item.id === product.id);
+      if (prd.qtyItem === 1) {
+        setCartState(cartState.filter((item) => item.id !== product.id));
+      } else {
+        setCartState(
+          cartState.map((item) =>
+            item.id === product.id
+              ? { ...item, qtyItem: item.qtyItem - 1 }
+              : item
+          )
+        );
+      }
+    }
   };
   return (
     <CartContext.Provider value={{ cartState, addItem, removeItem }}>
